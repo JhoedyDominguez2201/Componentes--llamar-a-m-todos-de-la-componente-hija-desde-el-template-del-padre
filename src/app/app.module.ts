48f30435dbd56d41e70bdf5db17d3d4c475b9736
JhoedyDominguez2201 <l17390333@chetumal.tecnm.mx>
2022-06-24 22:53:59 -0500

    Commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectornumericoComponent
   ],
   imports: [
     BrowserModule
